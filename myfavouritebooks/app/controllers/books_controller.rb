4a3304eac8538446ea2c78c0193e4d1c8432b318
Ditmar <didox222@gmail.com>
2018-01-20 19:49:32 +0000

    Filtering and sorting

--- myfavouritebooks/app/controllers/books_controller.rb
+++ myfavouritebooks/app/controllers/books_controller.rb
@@ -1,7 +1,31 @@
 class BooksController < ApplicationController
  
  def index
-  @books = Book.all
+  sort = params[:sort] || session[:sort]
+    case sort
+    when 'title'
+     ordering,@title_header = {:title => :asc}, 'hilite'
+    when 'publish_date'
+     ordering,@date_header = {:publish_date => :asc}, 'hilite'
+    when 'genre'
+     ordering,@genre_header = {:genre => :asc}, 'hilite'
+    end
+    
+    @all_genres = Book.all_genres
+    @selected_genres = params[:genres] || session[:genres] || {}
+     
+    if @selected_genres == {}
+      @selected_genres = Hash[@all_genres.map {|genre| [genre, genre]}]
+    end
+     
+    if params[:sort] != session[:sort] or params[:genres] != session[:genres]
+      session[:sort] = sort
+      session[:genres] = @selected_genres
+      redirect_to :sort => sort, :genres => @selected_genres and return
+    end
+    @books = Book.where(genre: @selected_genres.keys).order(ordering)
+  
+  
  end
  
  def show
--- myfavouritebooks/app/models/book.rb
+++ myfavouritebooks/app/models/book.rb
@@ -1,2 +1,3 @@
 class Book < ActiveRecord::Base
+    def self.all_genres ; %w[Science_Fiction Drama Action_And_Adventure Romance Mystery Horror] ; end #  shortcut: array of strings
 end
