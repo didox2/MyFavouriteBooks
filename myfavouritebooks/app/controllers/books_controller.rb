52e6fa2f9f41861ccafce1b18f82c8a5e198aef9
Ditmar <didox222@gmail.com>
2018-01-20 18:32:37 +0000

    Almost finished

--- myfavouritebooks/app/controllers/books_controller.rb
+++ myfavouritebooks/app/controllers/books_controller.rb
@@ -10,4 +10,36 @@ class BooksController < ApplicationController
   # will render app/views/books/show.html.haml by default
  end
  
+ def new
+  @book = Book.new
+  # default: render 'new' template
+ end
+ 
+ def create
+  params.require(:book)
+  permitted = params[:book].permit(:title,:genre,:publish_date,:description)
+  @book = Book.create!(permitted)
+  redirect_to books_path
+ end
+ 
+ def edit
+  @book = Book.find params[:id]
+ end
+ 
+ def update
+  @book = Book.find params[:id]
+  params.require(:book)
+  permitted = params[:book].permit(:title,:genre,:publish_date,:description)
+  @book.update_attributes!(permitted)
+  flash[:notice] = #{@book.title} was successfully updated.
+  redirect_to book_path(@book)
+ end
+ 
+ def destroy
+  @book = Book.find(params[:id])
+  @book.destroy
+  flash[:notice] = "Book '#{@book.title}' was deleted."
+  redirect_to books_path
+ end
+
 end
